3923e18ad1357b4eac8599065af0f50fd97d45e6
skylord <atulmehlasmart@gmail.com>
2019-08-22 07:23:33 -0230

    made few changes

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const canvas=document.getElementById("canvas")
+const canvas=document.querySelector("canvas")
 let c=canvas.getContext("2d");
-c.fillRect(0,0)
+canvas.height=innerHeight;
+canvas.width=window.innerWidth;
+window.addEventListener("resize",()=>{
+    canvas.height=innerHeight;
+canvas.width=window.innerWidth;
+    init()
+})
+
+
+function init(){
+    c.clearRect(0,0,innerWidth,innerHeight)
+    c.moveTo(0,canvas.height/2);
+    for (let i=0;i<canvas.width;i++){
+        c.lineTo(i,i);
+    }
+    c.stroke()
+}
+init()
